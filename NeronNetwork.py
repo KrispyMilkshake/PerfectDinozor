4f6e5f40ed945a5f298bcb455ca07abfd18f43ed
KrispyMilkshake <shlemut.glitters@gmail.com>
2022-12-03 15:26:40 +0200

    created nn class

---
+++ NeronNetwork.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+ReLU = lambda x: np.max(x, 0)
+
+
+def rand0m(size):
+    return np.random.random(size) * 2 - 1
+
+class NeronNetwork:
+
+    def __init__(self, weight_list, biest_list):
+        self.weight_list = weight_list
+        self.biest_list = biest_list
+
+    def forward_propagation(self, nn_input):
+        layer = nn_input
+
+        for i in range(len(self.weight_list) - 1):
+            layer = layer @ self.weight_list[i] + self.biest_list[i]
+            layer = ReLU(layer)
+
+        layer = layer @ self.weight_list[-1] + self.biest_list[-1]
+        layer = np.tanh(layer)
+        return layer
+
+
+if __name__ == '__main__':
+    weight_list = [rand0m((3, 10)), rand0m((10, 10)), rand0m((10, 2))]
+    biest_list = [rand0m((1, 10)), rand0m((1, 10)), rand0m((1, 2))]
+    nn = NeronNetwork(weight_list, biest_list)
+
+    nn_input = np.ones((3))
+    print(nn.forward_propagation(nn_input))
---
+++ main.py
@@ -0,0 +1,12 @@
+import NeronNetwork
+import numpy as np
+
+
+# def main():
+#     weight_list = [np.ones((3, 10)), np.ones((10, 10)), np.ones((10, 2))]
+#     biest_list = [np.zeros((1, 10)), np.zeros((1, 10)), np.zeros((1, 2))]
+#     nn = NeronNetwork(weight_list,biest_list)
+#
+#
+# if __name__ == '__main__':
+#     main()
