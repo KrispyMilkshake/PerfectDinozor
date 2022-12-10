2286555d3476e235e18ea626f77197fd8df809c4
edengranot <eden.granot@gmail.com>
2022-12-10 14:00:10 +0200

    nn dinozor

---
+++ dinozr_game/neron_network_dinozor.py
@@ -0,0 +1,20 @@
+import pygame
+
+from dinozr_game.dinozor import Dinozor
+from natural_selection.NeronNetwork import NeronNetwork
+
+
+class NeronNetworkDinozor(Dinozor):
+    def __init__(self, neron_network: NeronNetwork):
+        super().__init__()
+        self.neron_network = neron_network
+
+    def nn_update(self, game_speed, distance, obs_type):
+        neron_network_input = [game_speed, distance, obs_type]
+        update = self.neron_network.forward_propagation(neron_network_input)
+
+        user_input = {}
+        user_input[pygame.K_UP] = update[0] > 0.5
+        user_input[pygame.K_DOWN] = update[1] > 0.5
+
+        self.update(user_input)
