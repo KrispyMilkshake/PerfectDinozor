9d0766f48fbebaf8875f2c79c8916fd01c0e31e7
KrispyMilkshake <shlemut.glitters@gmail.com>
2023-01-07 17:01:38 +0200

    it wrks

--- dinozr_game/constants.py
+++ dinozr_game/constants.py
@@ -14,4 +14,3 @@ SCREEN_HEIGHT = 600
 SCREEN_WIDTH = 1100
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-#MAMAID_FONT = pygame.font.Font("Mermaid Babies.ttf", 20)
--- main.py
+++ main.py
@@ -3,14 +3,26 @@ from natural_selection.NeronNetworkBuilder import NeronNetworkBuilder
 from natural_selection.neron_network_learner import NeronNetworkLearner
 from dinozr_game import game
 
-
-size_list = [3, 4, 4, 2]
-nn_list = NeronNetworkBuilder.create_random_nn_list(size_list, 100)
+SIZE_LIST = [3, 4, 4, 2]
+NUMBER_OF_NN_IN_GENERATION = 100
 
 
 def main():
-    nn_player = NeronNetworkDinozor(nn_list[0])
-    print(game.game_2(nn_player))
+    nn_list = NeronNetworkBuilder.create_random_nn_list(SIZE_LIST, NUMBER_OF_NN_IN_GENERATION)
+    for _ in range(10):
+        nn_players = [NeronNetworkDinozor(i) for i in nn_list]
+        dic_nn_to_score = game.game(nn_players)
+
+
+
+        couples_nn_list = NeronNetworkLearner.choose_couples(dic_nn_to_score, NUMBER_OF_NN_IN_GENERATION)
+        new_nn_list = []
+        for couple in couples_nn_list:
+            new_nn_list.append(NeronNetworkBuilder.merge_two_nn(*couple))
+
+        nn_list = new_nn_list
+
+
 
 if __name__ == '__main__':
     main()
